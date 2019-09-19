3370e7a732a870aff00833f8a0354ef7f6d81909
marceldragon <ManUnited89>
2019-09-19 11:13:59 +0200

    wgranie projektu

---
+++ src/main/java/com/example/PostawIWygraj/PostawIWygrajApplication.java
@@ -0,0 +1,13 @@
+package com.example.PostawIWygraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostawIWygrajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostawIWygrajApplication.class, args);
+	}
+
+}
